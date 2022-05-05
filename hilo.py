a80321d99f3e16519552c7c92a75e4eb781d3c44
vdmorris <morrisvd@byui.edu>
2022-05-05 09:43:03 -0400

    set up all classes organized in meeting

--- hilo.py
+++ hilo.py
@@ -1,5 +1,24 @@
-class Get_Input:
+class Director:
+    #
+    def __init__(self):
+        card = self.Cards
+    
+    def get_input(self):
+        ''
+    
+    def update(self):
+        ''
+    
+    def do_output(self):
+        ''
+    
+    def start_game(self):
+        ''
+
+class Cards:
 
     def __init__(self):
-
-class do_Output:
+        ''
+    
+    def get_card(self):
+        ''
