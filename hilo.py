ac970691b709e53a17026710f17b7897927f265e
Clancy Thomas <clancyvthomas@gmail.com>
2022-05-11 22:03:47 -0600

    Finished card class

--- hilo.py
+++ hilo.py
@@ -1,5 +1,6 @@
 from random import seed
 from random import randint
+
 class Director:
     #
     def __init__(self):
@@ -23,22 +24,11 @@ class Director:
 class Cards:
 
     def __init__(self):
-<<<<<<< HEAD
         pass
-=======
-        self.first_card = ''
+        self.first_card = 0
         self.second_card = 0
     
-    def flip_first_card(self):
-        ''
->>>>>>> 674da11005762a8f75d43f2edaa3e35f4c09ffb4
-    
-<<<<<<< HEAD
-    def flip_second_card(self):
-=======
     def get_card(self):
-<<<<<<< HEAD
-<<<<<<< HEAD
         return randint(1,14)
 
 
@@ -47,24 +37,8 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-=======
->>>>>>> 211144ca85acbb4238637dbcd8639538324d3cfa
->>>>>>> 674da11005762a8f75d43f2edaa3e35f4c09ffb4
-        ''
-
 
 ''' DESIGN
-<<<<<<< HEAD
-class: Card
-    Responsibility: To hold card values
-        first_card: int
-        second_card: int
-    Behaviors: To get new card values
-        flip_first_card(): int
-        flip_second_card(): int
-class: Director
-=======
 
 class: Card
 
@@ -78,15 +52,10 @@ class: Card
 
 class: Director
 
->>>>>>> 211144ca85acbb4238637dbcd8639538324d3cfa
     Responsibility: To hold player score, win of round, and value of continue play
         player_score: int
         win_round: bool
         continue_playing: bool
-<<<<<<< HEAD
-=======
-
->>>>>>> 211144ca85acbb4238637dbcd8639538324d3cfa
     Beaviors: To evaluate card values, update score,
                 and prompt for continued play
         
@@ -95,4 +64,3 @@ class: Director
         update_score(): int score, bool win_round
         prompt_continue_playing(): bool continue_playing
 '''
->>>>>>> 211144ca85acbb4238637dbcd8639538324d3cfa
