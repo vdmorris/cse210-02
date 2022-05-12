03f088a26cd827d4c0b6703813715a8896046966
vdmorris <morrisvd@byui.edu>
2022-05-12 00:05:57 -0400

    "pre-Clancy change"

--- hilo.py
+++ hilo.py
@@ -1,42 +1,33 @@
+import random
+
 class Director:
-    #
-    def __init__(self):
-        card = self.Cards
-        self.player_score = 300
-        self.win_round = ''
-        self.continue_playing = ''
     
-    def get_card_info(self):
+    def get_card_info():
+        card1 = Cards.flip_first_card()
+        
+    
+    def evaulate_cards_values():
         ''
     
-    def evaulate_cards_values(self):
-        ''
+    def update_score():
+        score = 300
+
+
+        return score
     
-    def update_score(self):
-        ''
-    
-    def prompt_continue_playing(self):
+    def prompt_continue_playing():
         ''
 
 class Cards:
-
-    def __init__(self):
-        self.first_card = ''
-        self.second_card = 0
     
-    def flip_first_card(self):
+    def flip_first_card():
         ''
     
-<<<<<<< HEAD
-    def flip_second_card(self):
-=======
-    def get_card(self):
->>>>>>> 211144ca85acbb4238637dbcd8639538324d3cfa
+    def flip_second_card():
         ''
 
 
 ''' DESIGN
-<<<<<<< HEAD
 class: Card
     Responsibility: To hold card values
         first_card: int
@@ -45,29 +36,10 @@ class: Card
         flip_first_card(): int
         flip_second_card(): int
 class: Director
-=======
-
-class: Card
-
-    Responsibility: To hold card values
-        first_card: int
-        second_card: int
-
-    Behaviors: To get new card values
-        flip_first_card(): int
-        flip_second_card(): int
-
-class: Director
-
->>>>>>> 211144ca85acbb4238637dbcd8639538324d3cfa
     Responsibility: To hold player score, win of round, and value of continue play
         player_score: int
         win_round: bool
         continue_playing: bool
-<<<<<<< HEAD
-=======
-
->>>>>>> 211144ca85acbb4238637dbcd8639538324d3cfa
     Beaviors: To evaluate card values, update score,
                 and prompt for continued play
         
